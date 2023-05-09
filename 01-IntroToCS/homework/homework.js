e0fd390159822df50cdf110680ef130404aee93e
mccoronel <mcelestecrnl@gmail.com>
2023-05-09 13:32:29 -0300

    Homework completa

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -9,6 +9,18 @@ function BinarioADecimal(num) {
   //    1 * 2 ** 3  =  8
   //    1 * 2 ** 4  =  16
   //    0 * 2 ** 5  =   0
+  let inversa = num.toString().split('').reverse();
+    let resultado = 0;
+    for( let i = 0 ; i< inversa.length ; i++){
+      resultado = resultado+ inversa[i] * (2**i);
+    }
+      return resultado;
+     
+      // let inversa = num.toString().split('').reverse().map((numero, i) => numero * (2**i));
+      // return inversa.reduce((acc, numero) =>{
+      //    return acc+numero;
+      // },0)
+
 }
 
 function DecimalABinario(num) {
@@ -23,6 +35,16 @@ function DecimalABinario(num) {
   // 5 / 2 = 2 residuo 1
   // 2 / 2 = 1 residuo 0
   // 1 / 2 = 0 residuo 1
+  let binario = [];
+    
+   while(num > 0){
+       let resto = num % 2;
+       binario.unshift(resto);
+       num = Math.floor(num / 2);              
+   }
+  
+   return binario.join('')
+
 }
 
 module.exports = {
